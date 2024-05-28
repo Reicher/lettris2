25cf2f3cb96f00f378516d56f3e032ebf735bc76
Reicher <robin.o.reicher@gmail.com>
2024-05-28 21:17:05 +0200

    refactor and refine box placement

--- BoxGenerator2.gd
+++ BoxGenerator2.gd
@@ -4,26 +4,39 @@ extends Node2D
 var box_scene
 var rng = RandomNumberGenerator.new()
 
+var letterValues = {
+	'A': 1, 'B': 3, 'C': 3, 'D': 2, 'E': 1, 'F': 4, 'G': 2, 'H': 4,
+	'I': 1, 'J': 8, 'K': 5, 'L': 1, 'M': 3, 'N': 1, 'O': 1, 'P': 3,
+	'Q': 10, 'R': 1, 'S': 1, 'T': 1, 'U': 1, 'V': 4, 'W': 4, 'X': 8,
+	'Y': 4, 'Z': 10
+}
+
 func _ready():
 	box_scene = load("res://box.tscn")
 
+# A new box should be created
 func _on_timer_timeout():
-	# Get the viewport width and height
+		# Get the viewport width and height
 	var viewport_rect = get_viewport_rect()
 	var viewport_width = viewport_rect.size.x
+
+	# Check if any box is above the visible screen
+	for child in get_children():
+		if child.name != "Timer" and child.position.y < 0:
+			print("Game over")
 
 	# Instantiate the Box scene at the random position
 	var box = box_scene.instantiate()	
 	
-	# Generate random coordinates within the viewport width
-	box.position = Vector2(rng.randf_range(0, viewport_width), -30)
+	# Generate parameters for the box
+	var x_bounds = Vector2(box.size[0]/2, viewport_width - box.size[0]/2)
+	box.position = Vector2(rng.randf_range(x_bounds[0], x_bounds[1]), -30)
+	box.letter = letterValues.keys()[randi() % letterValues.size()]
+	box.value = letterValues[box.letter]
 	
 	# Ensure the UI node is correctly referenced
 	var ui_node = get_node("../Panel/UI")
-	if ui_node == null:
-		print("UI node not found!")
-		return
-	
+
 	# Connect signals
 	box.clicked.connect(ui_node.box_clicked)
 
--- box.gd
+++ box.gd
@@ -2,20 +2,12 @@ extends RigidBody2D
 
 signal clicked(box)
 
-var letterValues = {
-	'A': 1, 'B': 3, 'C': 3, 'D': 2, 'E': 1, 'F': 4, 'G': 2, 'H': 4,
-	'I': 1, 'J': 8, 'K': 5, 'L': 1, 'M': 3, 'N': 1, 'O': 1, 'P': 3,
-	'Q': 10, 'R': 1, 'S': 1, 'T': 1, 'U': 1, 'V': 4, 'W': 4, 'X': 8,
-	'Y': 4, 'Z': 10
-}
 var selected = false
 var letter = " "
 var value = 0
+var size = Vector2(80, 80) # Needs to be set dynamically later
 
 func _ready():
-	letter = letterValues.keys()[randi() % letterValues.size()]
-	value = letterValues[letter]
-	
 	$Letter.text = letter
 	$Value.text = str(value)
 
