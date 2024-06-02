91a79787ef91c5c280cf15bcab7b14fb90155535
Reicher <robin.o.reicher@gmail.com>
2024-06-02 20:15:13 +0200

    new way of randomizing letters

--- scripts/Game.gd
+++ scripts/Game.gd
@@ -8,23 +8,34 @@ var Ball = preload("res://scenes/boxes/Ball.tscn")
 
 var rng = RandomNumberGenerator.new()
 
-var letterValues = {
-	'A': 1, 'B': 3, 'C': 3, 'D': 2, 'E': 1, 'F': 4, 'G': 2, 'H': 4,
-	'I': 1, 'J': 8, 'K': 5, 'L': 1, 'M': 3, 'N': 1, 'O': 1, 'P': 3,
-	'Q': 10, 'R': 1, 'S': 1, 'T': 1, 'U': 1, 'V': 4, 'W': 4, 'X': 8,
-	'Y': 4, 'Z': 10
+var letterData = {
+	'A': { 'value': 1, 'quantity': 9 },  'B': { 'value': 3, 'quantity': 2 },
+	'C': { 'value': 3, 'quantity': 2 },  'D': { 'value': 2, 'quantity': 4 },
+	'E': { 'value': 1, 'quantity': 12 }, 'F': { 'value': 4, 'quantity': 2 },
+	'G': { 'value': 2, 'quantity': 3 },  'H': { 'value': 4, 'quantity': 2 },
+	'I': { 'value': 1, 'quantity': 9 },  'J': { 'value': 8, 'quantity': 1 },
+	'K': { 'value': 5, 'quantity': 1 },  'L': { 'value': 1, 'quantity': 4 },
+	'M': { 'value': 3, 'quantity': 2 },  'N': { 'value': 1, 'quantity': 6 },
+	'O': { 'value': 1, 'quantity': 8 },  'P': { 'value': 3, 'quantity': 2 },
+	'Q': { 'value': 10, 'quantity': 1 }, 'R': { 'value': 1, 'quantity': 6 },
+	'S': { 'value': 1, 'quantity': 4 },  'T': { 'value': 1, 'quantity': 6 },
+	'U': { 'value': 1, 'quantity': 4 },  'V': { 'value': 4, 'quantity': 2 },
+	'W': { 'value': 4, 'quantity': 2 },  'X': { 'value': 8, 'quantity': 1 },
+	'Y': { 'value': 4, 'quantity': 2 },  'Z': { 'value': 10, 'quantity': 1 }
 }
-var weightedList = [] # Used for deciding how often a letter should appear
-	
+
 func _ready():
-	# Create a list where each letter appears a number of times equal to its weight
-	for letter in letterValues:
-		for i in int(10 / letterValues[letter]):
-			weightedList.append(letter)
+	pass
 	
 func _get_semi_random_letter():
-	# Select a random element from the weighted list
-	return weightedList[randi() % weightedList.size()]
+	var random_number = randi() % (98 - 1) # Number of letter tiles
+	var count = 0
+	
+	# Find the letter corresponding to the random_number
+	for letter in letterData.keys():
+		count += letterData[letter]['quantity']
+		if count > random_number:
+			return letter
 	
 func _get_next_box_type():
 	var box_types = [Normal, Silver, Gold, Ball]	
@@ -32,7 +43,7 @@ func _get_next_box_type():
 
 # A new box should be created
 func _on_timer_timeout():
-		# Get the viewport width and height
+	# Get the viewport width and height
 	var viewport_rect = get_viewport_rect()
 	var viewport_width = viewport_rect.size.x
 
@@ -47,12 +58,10 @@ func _on_timer_timeout():
 	var x_bounds = Vector2(box.size[0]/2, viewport_width - box.size[0]/2)
 	box.position = Vector2(rng.randf_range(x_bounds[0], x_bounds[1]), -30)
 	box.letter = _get_semi_random_letter()
-	box.value = letterValues[box.letter]
+	box.value = letterData[box.letter].value
 	
-	# Ensure the UI node is correctly referenced
+	# Connect box click to UI
 	var ui_node = get_node("Panel/UI")
-
-	# Connect signals
 	box.clicked.connect(ui_node.box_clicked)
 
 	# Add the instantiated scene to the current scene
