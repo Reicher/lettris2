2ee16db8eefe8ef907d9275b9747a44caec5e1c5
Reicher <robin.o.reicher@gmail.com>
2024-06-05 23:22:58 +0200

    Fix a nice little fade

--- scripts/SplashScreen.gd
+++ scripts/SplashScreen.gd
@@ -3,5 +3,13 @@ extends Control
 
 var main_menu = preload("res://scenes/MainMenu.tscn")
 
-func _on_timer_timeout():
-	get_tree().change_scene_to_packed(main_menu)
+func _ready():
+	$AnimationPlayer.play("fade_from_black")
+	$AnimationPlayer.animation_finished.connect(_on_animation_finished)
+
+func _on_animation_finished(anim_name):
+	if anim_name == "fade_from_black":
+		$AnimationPlayer.play("fade_to_black")
+	elif anim_name == "fade_to_black":
+		get_tree().change_scene_to_packed(main_menu)
+		
