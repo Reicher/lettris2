035060f1cc9be35ec7c08a6ea66956a818eabf2b
Reicher <robin.o.reicher@gmail.com>
2024-09-10 21:57:18 +0200

    wops, forgot to save

--- scripts/GameOver.gd
+++ scripts/GameOver.gd
@@ -1,7 +1,5 @@
 extends Control
 
-var main_menu = preload("res://scenes/MainMenu.tscn")
-
 func _ready() -> void:	
 	var score_label = $"Outer Margin/Inner Margin/Most inner/TentBack/Score"
 	score_label.text = str(Global.score)
@@ -9,7 +7,4 @@ func _ready() -> void:
 	tent.play("open")
 
 func _on_back_button_pressed():
-	print(get_tree().change_scene_to_packed(main_menu))
-	print(get_tree().change_scene_to_file("res://scenes/MainMenu.tscn"))
-
-	print("THE FUCK")
+	get_tree().change_scene_to_file("res://scenes/MainMenu.tscn")
