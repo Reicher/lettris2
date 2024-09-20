ddebcfea11f4a40d104f8a221fe88f8a52ff2fd7
Reicher <robin.o.reicher@gmail.com>
2024-09-20 23:25:56 +0200

    Update lights
    Update lights
    
    took way to much time for such a small thing

--- scripts/light.gd
+++ scripts/light.gd
@@ -1,4 +1,15 @@
 extends Control
 
+var active = bool(randi() % 2)
+
 func _ready() -> void:
-	$AnimatedSprite2D.play("default", randf_range(0.1, 1))
+	$AnimatedSprite2D.frame = 0 if active else 2
+	$Timer.wait_time = randf_range(0.3, 1)
+	$Timer.start()
+
+func _on_timer_timeout() -> void:
+	$AnimatedSprite2D.play("off" if active else "on")
+	$AnimatedSprite2D.connect("animation_finished", finish_toggle)
+	
+func finish_toggle() -> void: 
+	active = !active
